0acee0bf30996250f8018c9907a74163b9ae6666
Kirill Strelkov <kirill.strelkov@github.com>
2020-05-03 18:28:49 +0200

    update version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(
 
 setup(
     name="easyselenium",
-    version="0.4.1",
+    version="0.4.2",
     description="easyselenium - Selenium-based Test Automation Framework",
     author="Kirill Strelkov",
     author_email="kirillstrelkov@users.noreply.github.com",
